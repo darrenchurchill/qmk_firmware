626b77f7298f8f3bb607ac76dd165352a7a561a1
Darren Churchill <darrenmchurchill@gmail.com>
2023-01-01 10:29:07 -0800

    atreus: Initial atreus config.

---
+++ keyboards/keyboardio/atreus/keymaps/darrenchurchill/keymap.c
@@ -0,0 +1,50 @@
+/*
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include QMK_KEYBOARD_H
+
+
+enum layer_names {
+    _QW,
+    _RS,
+    _LW,
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  [_QW] = LAYOUT( /* Qwerty */
+    KC_Q,         KC_W,        KC_E,        KC_R,        KC_T,                                   KC_Y,   KC_U,            KC_I,        KC_O,        KC_P,
+    LCTL_T(KC_A), ALT_T(KC_S), SFT_T(KC_D), GUI_T(KC_F), KC_G,                                   KC_H,   GUI_T(KC_J),     SFT_T(KC_K), ALT_T(KC_L), CTL_T(KC_SCLN),
+    KC_Z,         KC_X,        KC_C,        KC_V,        KC_B,    KC_GRV,         KC_BSLS,       KC_N,   KC_M,            KC_COMM,     KC_DOT,      KC_SLSH,
+    KC_LCTL,      KC_LALT,     KC_LGUI,     MO(_LW),     KC_BSPC, SFT_T(KC_ESC),  SFT_T(KC_ENT), KC_SPC, LT(_RS, KC_TAB), KC_QUOT,     KC_MINS,     HYPR_T(KC_ENT)
+    ),
+
+  [_LW] = LAYOUT( /* [> Lower <] */
+    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,     KC_8,     KC_9,    KC_0,
+    KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC, KC_AMPR,  KC_ASTR,  KC_LPRN, KC_RPRN,
+    KC_LCBR, KC_RCBR, KC_UNDS, KC_PLUS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MINUS, KC_EQUAL, KC_LBRC, KC_RBRC,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS, CW_TOGG
+    ),
+
+  [_RS] = LAYOUT( /* [> Raise <] */
+    KC_TRNS, KC_TRNS, KC_KB_VOLUME_UP,   KC_TRNS, KC_TRNS,                   KC_HOME, KC_PGDN,       KC_PGUP,       KC_END,   KC_TRNS,
+    KC_TRNS, KC_MPRV, KC_KB_VOLUME_DOWN, KC_MNXT, KC_TRNS,                   KC_LEFT, KC_DOWN,       KC_UP,         KC_RIGHT, KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_MUTE,           KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, SGUI(KC_LBRC), SGUI(KC_RBRC), KC_TRNS,  KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_TRNS,           KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,       KC_TRNS,       KC_TRNS,  KC_MPLY
+    )
+};
+
+qk_tap_dance_action_t tap_dance_actions[] = {
+
+};
