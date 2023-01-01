e407c05e3208a6f023f22ceebc09e3eaac6486aa
Darren Churchill <darrenmchurchill@gmail.com>
2023-01-01 10:29:07 -0800

    atreus: Initial atreus config.

---
+++ keyboards/keyboardio/atreus/keymaps/darrenchurchill/config.h
@@ -0,0 +1,8 @@
+/*
+  Set any config.h overrides for your specific keymap here.
+  See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
+*/
+#define IGNORE_MOD_TAP_INTERRUPT
+#undef TAPPING_TERM
+#define TAPPING_TERM 225
+#define CAPS_WORD_IDLE_TIMEOUT 3000  // 3 seconds.
