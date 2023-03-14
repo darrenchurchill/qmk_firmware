45fcd11bc2ba0667c6ff93029df130d058ae0be8
Darren Churchill <darrenmchurchill@gmail.com>
2023-03-14 15:30:33 -0700

    atreus: Remove thumb shift and add CAPS WORD toggle to raise layer
    This also moves some of the raise layer keys to uncover Left shift on "C"

--- keyboards/keyboardio/atreus/keymaps/darrenchurchill/keymap.c
+++ keyboards/keyboardio/atreus/keymaps/darrenchurchill/keymap.c
@@ -27,7 +27,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_Q,          KC_W,          KC_E,        KC_R,        KC_T,                             KC_Y,   KC_U,        KC_I,           KC_O,          KC_P,
     KC_A,          KC_S,          KC_D,        KC_F,        KC_G,                             KC_H,   KC_J,        KC_K,           KC_L,          KC_SCLN,
     LCTL_T(KC_Z),  ALT_T(KC_X),   SFT_T(KC_C), GUI_T(KC_V), KC_B,    HYPR_T(KC_GRV), KC_MINS, KC_N,   GUI_T(KC_M), SFT_T(KC_COMM), ALT_T(KC_DOT), CTL_T(KC_SLSH),
-    CTL_T(KC_GRV), ALT_T(KC_ESC), KC_TAB,      MO(_RS),     MO(_LW), KC_LGUI,        KC_BSPC, KC_SPC, KC_LSFT,     KC_ENT,         KC_QUOT,       KC_DQUO
+    CTL_T(KC_GRV), ALT_T(KC_ESC), KC_TAB,      KC_LGUI,     MO(_LW), KC_LGUI,        KC_BSPC, KC_SPC, MO(_RS),     KC_ENT,         KC_QUOT,       KC_DQUO
     ),
 
   [_LW] = LAYOUT( /* [> Lower <] */
@@ -38,10 +38,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
   [_RS] = LAYOUT( /* [> Raise <] */
-    KC_TRNS, KC_TRNS, KC_KB_VOLUME_UP,   KC_TRNS, KC_TRNS,                   KC_HOME, KC_PGDN,       KC_PGUP,       KC_END,   KC_TRNS,
+    KC_MUTE, KC_TRNS, KC_KB_VOLUME_UP,   KC_TRNS, KC_TRNS,                   KC_HOME, KC_PGDN,       KC_PGUP,       KC_END,   KC_MPLY,
     KC_TRNS, KC_MPRV, KC_KB_VOLUME_DOWN, KC_MNXT, KC_TRNS,                   KC_LEFT, KC_DOWN,       KC_UP,         KC_RIGHT, KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_MUTE,           KC_TRNS, QK_BOOT, QK_RBT,  KC_TRNS, KC_TRNS, SGUI(KC_LBRC), SGUI(KC_RBRC), KC_TRNS,  KC_TRNS,
-    NK_ON,   NK_OFF,  KC_TRNS,           KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,       KC_TRNS,       KC_TRNS,  KC_MPLY
+    KC_TRNS, KC_TRNS, KC_TRNS,           KC_TRNS, QK_BOOT, QK_RBT,  KC_TRNS, KC_TRNS, SGUI(KC_LBRC), SGUI(KC_RBRC), KC_TRNS,  KC_TRNS,
+    CW_TOGG, NK_OFF,  KC_TRNS,           KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,       KC_TRNS,       KC_TRNS,  KC_TRNS
     )
 };
 
