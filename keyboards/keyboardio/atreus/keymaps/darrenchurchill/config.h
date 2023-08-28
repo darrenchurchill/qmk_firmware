545f67bab50a49cc487b7b8c5e227001d7e5b59f
Darren Churchill <darrenmchurchill@gmail.com>
2023-08-28 10:34:09 -0700

    atreus: use userspace config

--- keyboards/keyboardio/atreus/keymaps/darrenchurchill/config.h
+++ keyboards/keyboardio/atreus/keymaps/darrenchurchill/config.h
@@ -2,19 +2,3 @@
   Set any config.h overrides for your specific keymap here.
   See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
 */
-#undef TAPPING_TERM
-#define TAPPING_TERM 170
-#define PERMISSIVE_HOLD
-#define CAPS_WORD_IDLE_TIMEOUT 3000  // 3 seconds.
-
-#undef MOUSEKEY_WHEEL_DELAY
-#define MOUSEKEY_WHEEL_DELAY 40
-
-#undef MOUSEKEY_WHEEL_INTERVAL
-#define MOUSEKEY_WHEEL_INTERVAL 90
-
-#undef MOUSEKEY_WHEEL_MAX_SPEED
-#define MOUSEKEY_WHEEL_MAX_SPEED 1
-
-#undef MOUSEKEY_WHEEL_TIME_TO_MAX
-#define MOUSEKEY_WHEEL_TIME_TO_MAX 80
