8ebf0bf4dcbc23a67602cb619a4c65358d9ffb9c
Darren Churchill <darrenmchurchill@gmail.com>
2023-08-28 10:32:37 -0700

    userspace: add initial shared config files

---
+++ users/darrenchurchill/darrenchurchill.c
@@ -0,0 +1,85 @@
+#include QMK_KEYBOARD_H
+#include "darrenchurchill.h"
+
+
+__attribute__ ((weak))
+void keyboard_post_init_keymap(void) {
+    return;
+}
+
+void keyboard_post_init_user(void) {
+    keyboard_post_init_keymap();
+}
+
+
+#ifdef TAPPING_TERM_PER_KEY
+uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case LSFT_T(KC_C):
+        case RSFT_T(KC_COMM):
+            return g_tapping_term - 30;
+        default:
+            return g_tapping_term;
+    }
+}
+#endif // TAPPING_TERM_PER_KEY
+
+
+#ifdef TAP_DANCE_ENABLE
+typedef struct {
+    bool is_press_action;
+    uint8_t step;
+} tap;
+
+enum {
+    SINGLE_TAP = 1,
+    SINGLE_HOLD,
+    DOUBLE_TAP,
+    DOUBLE_HOLD,
+    DOUBLE_SINGLE_TAP,
+    MORE_TAPS
+};
+
+static tap dance_state[1];
+
+uint8_t dance_step(tap_dance_state_t *state);
+
+uint8_t dance_step(tap_dance_state_t *state) {
+    if (state->count == 1) {
+        if (state->interrupted || !state->pressed)
+            return SINGLE_TAP;
+        else
+            return SINGLE_HOLD;
+    } else if (state->count == 2) {
+        if (state->interrupted)
+            return DOUBLE_SINGLE_TAP;
+        else if (state->pressed)
+            return DOUBLE_HOLD;
+        else
+            return DOUBLE_TAP;
+    }
+    return MORE_TAPS;
+}
+
+void dance_0_finished(tap_dance_state_t *state, void *user_data);
+void dance_0_reset(tap_dance_state_t *state, void *user_data);
+
+void dance_0_finished(tap_dance_state_t *state, void *user_data) {
+    dance_state[0].step = dance_step(state);
+    switch (dance_state[0].step) {
+        case DOUBLE_TAP:
+            layer_move(_LAYERS);
+            break;
+    }
+}
+
+void dance_0_reset(tap_dance_state_t *state, void *user_data) {
+    wait_ms(10);
+    switch (dance_state[0].step) {}
+    dance_state[0].step = 0;
+}
+
+tap_dance_action_t tap_dance_actions[] = {
+    [DANCE_0] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, dance_0_finished, dance_0_reset),
+};
+#endif // TAP_DANCE_ENABLE
---
+++ users/darrenchurchill/readme.md
@@ -0,0 +1,16 @@
+# Darren's userspace config
+
+Shared stuff between keyboards.
+
+## Defined in `darrenchurchill.h`
+
+- Tap dance keycodes
+- Layer enumerators
+- Keycode wrappers
+- RGB matrix color wrappers
+
+## Defined in `darrenchurchill.c`
+
+- `void keyboard_post_init_keymap(void)`
+  - To be used in `keymap.c` in place of `keyboard_post_init_user()`
+- Tap dance functions
