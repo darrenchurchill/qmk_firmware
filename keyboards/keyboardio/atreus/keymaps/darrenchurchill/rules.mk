c31ed39c1520aaddc63e2a0f814dbc612cce3927
Darren Churchill <darrenmchurchill@gmail.com>
2023-08-28 10:34:09 -0700

    atreus: use userspace config

--- keyboards/keyboardio/atreus/keymaps/darrenchurchill/rules.mk
+++ keyboards/keyboardio/atreus/keymaps/darrenchurchill/rules.mk
@@ -1,7 +1,4 @@
 # Set any rules.mk overrides for your specific keymap here.
 # See rules at https://docs.qmk.fm/#/config_options?id=the-rulesmk-file
-MAGIC_ENABLE = yes
-TAP_DANCE_ENABLE = yes
-SPACE_CADET_ENABLE = no
-CAPS_WORD_ENABLE = yes
-DYNAMIC_TAPPING_TERM_ENABLE = yes
+CONSOLE_ENABLE = no
+COMMAND_ENABLE = no
