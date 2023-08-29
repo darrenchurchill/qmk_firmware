d47bb08e1016c96ba1f08edf87b23ee68f67cbc7
Darren Churchill <darrenmchurchill@gmail.com>
2023-08-29 11:18:24 -0700

    userspace combos: rename combo1 to combo_lower_space

--- users/darrenchurchill/combos.c
+++ users/darrenchurchill/combos.c
@@ -7,8 +7,8 @@
 #include "combos.h"
 #include "darrenchurchill.h"
 
-const uint16_t PROGMEM combo1[] = { LOWER, KC_SPC, COMBO_END};
+const uint16_t PROGMEM combo_lower_space[] = { LOWER, KC_SPC, COMBO_END};
 
 combo_t key_combos[] = {
-    COMBO(combo1, QK_REPEAT_KEY),
+    COMBO(combo_lower_space, QK_REPEAT_KEY),
 };
