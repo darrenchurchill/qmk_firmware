b8f10b28a58fc4ac3097922e6b044ffebed163f0
Darren Churchill <darrenmchurchill@gmail.com>
2023-08-29 13:28:42 -0700

    userspace: add hj: enter combo

--- users/darrenchurchill/combos.c
+++ users/darrenchurchill/combos.c
@@ -11,10 +11,12 @@ const uint16_t PROGMEM combo_lower_space[] = { LOWER, KC_SPC, COMBO_END};
 const uint16_t PROGMEM combo_lower_raise[] = { LOWER, RAISE, COMBO_END};
 const uint16_t PROGMEM combo_df[] = { KC_D, KC_F, COMBO_END};
 const uint16_t PROGMEM combo_fg[] = { KC_F, KC_G, COMBO_END};
+const uint16_t PROGMEM combo_hj[] = { KC_H, KC_J, COMBO_END};
 
 combo_t key_combos[] = {
     COMBO(combo_lower_space, QK_REPEAT_KEY),
     COMBO(combo_lower_raise, QK_ALT_REPEAT_KEY),
     COMBO(combo_df, KC_ESC),
-    COMBO(combo_fg, KC_TAB)
+    COMBO(combo_fg, KC_TAB),
+    COMBO(combo_hj, KC_ENTER)
 };
