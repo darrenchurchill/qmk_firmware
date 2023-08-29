3e3bfbb99bce99bffe4b9fb4237614518099d13c
Darren Churchill <darrenmchurchill@gmail.com>
2023-08-29 11:16:34 -0700

    userspace combos: remove non-functional Hyper/Meh combo

--- users/darrenchurchill/combos.c
+++ users/darrenchurchill/combos.c
@@ -7,10 +7,8 @@
 #include "combos.h"
 #include "darrenchurchill.h"
 
-const uint16_t PROGMEM combo0[] = { KC_MEH, KC_HYPR, COMBO_END};
 const uint16_t PROGMEM combo1[] = { LOWER, KC_SPC, COMBO_END};
 
 combo_t key_combos[] = {
-    COMBO(combo0, KC_CAPS_LOCK),
     COMBO(combo1, QK_REPEAT_KEY),
 };
