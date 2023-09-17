61f9be20a62889fd09beeeec17c4260903f77058
Darren Churchill <darrenmchurchill@gmail.com>
2023-09-17 16:39:11 -0700

    usrspc combos: change DF->Tab & move ESC to FG

--- users/darrenchurchill/combos.c
+++ users/darrenchurchill/combos.c
@@ -11,9 +11,9 @@
 // See config.h COMBO_ONLY_FROM_LAYER
 
 enum combos {
-    DF_ESC,
     JK_ENT,
-    MCOMMA_TAB,
+    DF_TAB,
+    FG_ESC,
     CVB_OS_COPY,
     VB_OS_PASTE,
     DK_CW_TOGG,
@@ -21,8 +21,8 @@ enum combos {
 
 
 const uint16_t PROGMEM combo_df[] = { LSFT_T(KC_D), LGUI_T(KC_F), COMBO_END};
+const uint16_t PROGMEM combo_fg[] = { LGUI_T(KC_F), KC_G, COMBO_END};
 const uint16_t PROGMEM combo_jk[] = { RGUI_T(KC_J), LSFT_T(KC_K), COMBO_END};
-const uint16_t PROGMEM combo_mcomma[] = { KC_M, KC_COMMA, COMBO_END};
 
 const uint16_t PROGMEM combo_cvb[] = { KC_C, KC_V, KC_B, COMBO_END};
 const uint16_t PROGMEM combo_vb[] = { KC_V, KC_B, COMBO_END};
@@ -31,9 +31,9 @@ const uint16_t PROGMEM combo_dk[] = { LSFT_T(KC_D), LSFT_T(KC_K), COMBO_END};
 
 
 combo_t key_combos[] = {
-    [DF_ESC] = COMBO(combo_df, KC_ESC),
+    [DF_TAB] = COMBO(combo_df, KC_TAB),
+    [FG_ESC] = COMBO(combo_fg, KC_ESC),
     [JK_ENT] = COMBO(combo_jk, KC_ENT),
-    [MCOMMA_TAB] = COMBO(combo_mcomma, KC_TAB),
     [CVB_OS_COPY] = COMBO(combo_cvb, UKC_OS_COPY),
     [VB_OS_PASTE] = COMBO(combo_vb, UKC_OS_PASTE),
     [DK_CW_TOGG] = COMBO(combo_dk, CW_TOGG),
@@ -45,7 +45,7 @@ combo_t key_combos[] = {
 // https://docs.qmk.fm/#/feature_combo?id=per-combo-timing-holding-tapping-and-key-press-order
 bool get_combo_must_tap(uint16_t index, combo_t *combo) {
     switch (index) {
-      case MCOMMA_TAB:
+      case DF_TAB:
         return false;
     }
 
