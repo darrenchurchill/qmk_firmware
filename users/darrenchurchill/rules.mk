0395d3789d0e5ba5b2a757847df03f5a43daf3e7
Darren Churchill <darrenmchurchill@gmail.com>
2023-10-15 15:40:17 -0700

    Disable auto shift
    - I'm not using it.
    - It's faster and easy enough to get shifted symbols on LWR layer with
      Shift sitting adjacent to the LWR mod tap home row keys.
    - It prevents RShift from functioning properly to trigger shifted combo
      keys. (see db54d7b9)

--- users/darrenchurchill/rules.mk
+++ users/darrenchurchill/rules.mk
@@ -12,7 +12,6 @@ LTO_ENABLE = yes
 # Set any rules.mk overrides for your specific user here.
 # See rules at https://docs.qmk.fm/#/config_options?id=the-rulesmk-file
 SPACE_CADET_ENABLE = no
-AUTO_SHIFT_ENABLE = yes
 OS_DETECTION_ENABLE = yes
 
 DYNAMIC_TAPPING_TERM_ENABLE = yes
