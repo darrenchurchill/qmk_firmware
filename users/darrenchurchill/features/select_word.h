48b5fbce1f163928676ebc484d23ac9b3127b887
Darren Churchill <darrenmchurchill@gmail.com>
2023-10-28 12:37:11 -0700

    select word: optionally handle a select line keycode
    This allows you to avoid having to modify the select word keycode with
    the shift modifier, which is sometimes annoying if the keycode isn't on
    the default layer.

--- users/darrenchurchill/features/select_word.h
+++ users/darrenchurchill/features/select_word.h
@@ -42,9 +42,14 @@
 extern "C" {
 #endif
 
-/** Handler function for select word. */
+/** Handler functions for select word. */
 bool process_select_word(uint16_t keycode, keyrecord_t* record,
                          uint16_t sel_keycode);
+
+bool process_select_word_or_line(uint16_t keycode,
+                                 keyrecord_t* record,
+                                 uint16_t sel_keycode,
+                                 uint16_t sel_line_keycode);
 
 /**
  * @fn select_word_task(void)
