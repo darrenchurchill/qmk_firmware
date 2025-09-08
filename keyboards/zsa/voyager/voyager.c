9911cc7484b0568b13ae43c65255fe9cc7a657ec
Darren Churchill <darrenmchurchill@gmail.com>
2025-09-07 20:18:02 -0700

    voyager: add rgb_matrix_enable eeprom config state
    This allows disabling the RGB animation so your per layer colors will
    display.

--- keyboards/zsa/voyager/voyager.c
+++ keyboards/zsa/voyager/voyager.c
@@ -240,6 +240,7 @@ bool process_record_kb(uint16_t keycode, keyrecord_t *record) {
             if (record->event.pressed) {
                 keyboard_config.disable_layer_led ^= 1;
                 if (keyboard_config.disable_layer_led) rgb_matrix_set_color_all(0, 0, 0);
+                eeconfig_update_kb(keyboard_config.raw);
             }
             break;
         case RGB_TOG:
@@ -247,12 +248,15 @@ bool process_record_kb(uint16_t keycode, keyrecord_t *record) {
                 switch (rgb_matrix_get_flags()) {
                     case LED_FLAG_ALL: {
                         rgb_matrix_set_flags(LED_FLAG_NONE);
+                        keyboard_config.rgb_matrix_enable = false;
                         rgb_matrix_set_color_all(0, 0, 0);
                     } break;
                     default: {
                         rgb_matrix_set_flags(LED_FLAG_ALL);
+                        keyboard_config.rgb_matrix_enable = true;
                     } break;
                 }
+                eeconfig_update_kb(keyboard_config.raw);
             }
             return false;
 #endif
